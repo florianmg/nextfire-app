84bbd90d4f62a8be6b361558b81142cb63ab9e8d
Florian <mesagouiffes@gmail.com>
2021-04-04 16:45:14 +0200

    Loader component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
-import Head from 'next/head'
+import Loader from '../components/Loader'
 
 export default function Home() {
   return (
     <main>
       <h1>Home page</h1>
+      <Loader show />
     </main>
   )
 }

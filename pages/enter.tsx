82f72a7eb426e44a70c8f01a8d8e817d0fe6517e
Florian <mesagouiffes@gmail.com>
2021-04-04 21:40:03 +0200

    auth login front

--- pages/enter.tsx
+++ pages/enter.tsx
@@ -1,11 +1,45 @@
 import React from 'react';
 
+import { auth, googleAuthProvider } from '../lib/firebase';
+
 const EnterPage: React.FC = () => {
+
+  const user = null;
+  const username = null;
+
   return(
     <main>
       <h1>Sign Up</h1>
+      {user ? 
+        !username ? <UsernameForm /> : <SignOutButton />
+        :
+        <SignInButton />
+      }
     </main>
   )
 };
+
+const UsernameForm: React.FC = () => {
+  return null;
+}
+
+const SignInButton: React.FC = () => {
+  const signInWithGoogle = async () => {
+    await auth.signInWithPopup(googleAuthProvider);
+  }
+  return (
+    <button className="btn-google" onClick={signInWithGoogle}>
+      <img src="/logo-google.png" /> Se connecter avec Google
+    </button>
+  );
+}
+
+const SignOutButton: React.FC = () => {
+  return (
+    <button onClick={() => auth.signOut()}>
+      Se déconnecter
+    </button>
+  );
+}
 
 export default EnterPage;

f6a796733447b52ee3a3a971f6cef6ead5a92bd9
Florian <mesagouiffes@gmail.com>
2021-04-11 15:03:14 +0200

    Feed on homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,72 @@
-import toast from 'react-hot-toast'
+import React, { useState } from 'react'
 
-export default function Home() {
+import PostFeed from '../components/PostFeed'
+import Loader from '../components/Loader'
+
+import { firestore, fromMillis, postsToJSON } from '../lib/firebase'
+
+import { Post } from '../types/posts.types'
+
+const MAX_POSTS_PER_QUERY = 1
+
+type HomeProps = {
+  posts: Post[]
+}
+
+export async function getServerSideProps(context) {
+  const postsQuery = firestore
+    .collectionGroup('posts')
+    .where('published', '==', true)
+    .orderBy('createdAt', 'desc')
+    .limit(MAX_POSTS_PER_QUERY)
+
+  const posts = (await postsQuery.get()).docs.map(postsToJSON)
+
+  // props passed to the component
+  return {
+    props: { posts },
+  }
+}
+
+const Home: React.FC<HomeProps> = ({ posts }) => {
+  const [loadedPosts, setLoadedPosts] = useState<Post[]>(posts)
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+  const [isPostsEnd, setIsPostsEnd] = useState<boolean>(false)
+
+  const getMorePosts = async () => {
+    setIsLoading(true)
+    const lastLoadedPost = loadedPosts[loadedPosts.length - 1]
+    const cursor =
+      typeof lastLoadedPost.createdAt === 'number'
+        ? fromMillis(lastLoadedPost.createdAt)
+        : lastLoadedPost.createdAt
+
+    const query = firestore
+      .collectionGroup('posts')
+      .where('published', '==', true)
+      .orderBy('createdAt', 'desc')
+      .startAfter(cursor)
+      .limit(MAX_POSTS_PER_QUERY)
+
+    const newPosts = (await query.get()).docs.map(postsToJSON)
+
+    setLoadedPosts(loadedPosts.concat(newPosts))
+    setIsLoading(false)
+
+    if (newPosts.length < MAX_POSTS_PER_QUERY) {
+      setIsPostsEnd(true)
+    }
+  }
   return (
     <main>
-      <h1>Home page</h1>
-      <button onClick={() => toast.success('You did it !')}>
-        Test (or toast) me
-      </button>
+      <PostFeed posts={loadedPosts} />
+      {!isLoading && !isPostsEnd && (
+        <button onClick={getMorePosts}>Voir plus</button>
+      )}
+      <Loader show={isLoading} />
+      {isPostsEnd && <p>Désolé mais vous avez tout lue !</p>}
     </main>
   )
 }
+
+export default Home
